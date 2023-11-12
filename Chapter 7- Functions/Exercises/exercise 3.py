71853070e1c5d193e3e61812a72d5a47317cd352
n1g3rr <147933522+n1g3rr@users.noreply.github.com>
2023-11-12 03:55:08 -0800

    Chapter 7
    all exercises

---
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -0,0 +1,8 @@
+def make_shirt(size, message):
+    print(f"Making a shirt of size {size} with the message: '{message}'")
+
+#Function called by using arguments
+make_shirt("medium", "Hello, World!")
+
+# Called the function a second time
+make_shirt(size="large", message="Python is awesome!")
